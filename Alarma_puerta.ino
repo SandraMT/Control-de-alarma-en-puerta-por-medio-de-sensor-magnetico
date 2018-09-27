1323344a511a2712b655c11974f8d3a5324a464e
SandraMT <43126177+SandraMT@users.noreply.github.com>
2018-09-27 09:46:18 -0500

    Alarma_potenciometro

--- Alarma_puerta.ino
+++ Alarma_puerta.ino
@@ -2,39 +2,49 @@ int sensor = 2;
 int led = 4;
 
 const int ALARMA = 3;   //buzzer
+
+int potenciometro = A1;
+int valor_pote = 0;
+
  
 void setup() {
-  //configurar pin como entrada con resistencia pull-up interna
   pinMode(sensor, INPUT_PULLUP);
   pinMode(led, OUTPUT);
   pinMode(ALARMA, OUTPUT);
-}
+ 
+ }
  
 void loop() {
+/* si el sensor está activado, quiere decir que la puerta está cerrada,
+    por lo tanto la alarma esta apagada, al abrirse la puerta
+    se abre el sensor y se enciende la alarma*/
   int value = digitalRead(sensor);
-
- /* si el sensor está activado, quiere decir que la puerta está cerrada,
-    por lo tanto el led debe de estar apagado, al abrirse la puerta
-    se abre el sensor y se enciende el led*/
     
-  if (value == LOW) {
-    digitalWrite(led, LOW);   
-  } else {
-    digitalWrite(led, HIGH);
+  if (value == HIGH){
+       Alarma();
   }
-   delay(500);    //control
-
-  if ( value == HIGH)
-     Alarma();
+  
 }
 
 void Alarma (){
+  /* Dependiendo del valor del potenciometro aumentará o disminuirá 
+   la velocidad del sonido, esto para indicar el estado de emergencia.*/ 
    
-  tone(ALARMA, 261.63);
-  delay(250);
+  valor_pote = analogRead(potenciometro);
+ 
+  
+  tone(ALARMA, 200);
+  delay(valor_pote);  
+  digitalWrite(led,HIGH);
  
   noTone(ALARMA);
-  //delay(300);
+  delay(valor_pote);   
+  digitalWrite(led,LOW);
 
 }
+
+
+
+
+
 
