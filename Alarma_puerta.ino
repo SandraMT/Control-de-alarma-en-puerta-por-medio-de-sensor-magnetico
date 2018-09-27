5a316a031206c1c1d648e9593b9b13b117fe709a
SandraMT <43126177+SandraMT@users.noreply.github.com>
2018-09-27 09:45:20 -0500

    Puerta_sensor_alarma

--- Alarma_puerta.ino
+++ Alarma_puerta.ino
@@ -1,27 +1,40 @@
-const int sensor = 2;
-const int LED = 4;
+int sensor = 2;
+int led = 4;
+
+const int ALARMA = 3;   //buzzer
  
 void setup() {
   //configurar pin como entrada con resistencia pull-up interna
   pinMode(sensor, INPUT_PULLUP);
-  pinMode(LED, OUTPUT);
+  pinMode(led, OUTPUT);
+  pinMode(ALARMA, OUTPUT);
 }
  
 void loop() {
-  control_sensor();
+  int value = digitalRead(sensor);
+
+ /* si el sensor está activado, quiere decir que la puerta está cerrada,
+    por lo tanto el led debe de estar apagado, al abrirse la puerta
+    se abre el sensor y se enciende el led*/
+    
+  if (value == LOW) {
+    digitalWrite(led, LOW);   
+  } else {
+    digitalWrite(led, HIGH);
+  }
+   delay(500);    //control
+
+  if ( value == HIGH)
+     Alarma();
 }
 
-void control_sensor(){
-  int value = digitalRead(sensor);
+void Alarma (){
+   
+  tone(ALARMA, 261.63);
+  delay(250);
  
-  if (value == LOW) {
-    /* si el sensor está activado, quiere decir que la puerta está cerrada,
-    por lo tanto el led debe de estar apagado, al abrirse la puera, se enciende el led*/
-    digitalWrite(LED, LOW);   
-  } else {
-    digitalWrite(LED, HIGH);
-  }
- 
-  delay(1000);
+  noTone(ALARMA);
+  //delay(300);
+
 }
 
